41e6fcd183f6e0441ee61be230babd790a3e817d
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-08-07 16:52:22 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,3 +3,27 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const decrementButton = document.querySelector('[data-action="decrement"]');
+
+const incrementButton = document.querySelector('[data-action="increment"]');
+
+const valueElement = document.querySelector("#value");
+
+let counterValue = 0;
+
+function updateCounter() {
+  valueElement.textContent = counterValue;
+}
+
+decrementButton.addEventListener("click", () => {
+  counterValue -= 1;
+  updateCounter();
+});
+
+incrementButton.addEventListener("click", () => {
+  counterValue += 1;
+  updateCounter();
+});
+
+updateCounter();
