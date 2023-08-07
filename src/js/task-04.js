b479947e604641a0a9e56bcd19c8dbd4f8436869
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-08-07 17:38:59 +0300

    Updates task 04 and task 05

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,9 +1,3 @@
-// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
-
-// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
-// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
-// Оновлюй інтерфейс новим значенням змінної counterValue.
-
 const decrementButton = document.querySelector('[data-action="decrement"]');
 
 const incrementButton = document.querySelector('[data-action="increment"]');
@@ -25,5 +19,3 @@ incrementButton.addEventListener("click", () => {
   counterValue += 1;
   updateCounter();
 });
-
-updateCounter();
---
+++ src/js/task-05.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", () => {
+  const enteredName = nameInput.value.trim();
+
+  nameOutput.textContent = enteredName === "" ? "Anonymous" : enteredName;
+});
