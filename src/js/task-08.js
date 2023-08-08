9a49f729b353530a072313f35f65ad37f2739de6
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-08-08 22:17:41 +0300

    Updates task-08.js, task-09.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -5,3 +5,50 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+// const loginForm = document.querySelector(".login-form");
+// const emailInput = loginForm.elements.email;
+// const passwordInput = loginForm.elements.password;
+
+// loginForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   if (emailInput.value === "" || passwordInput.value === "") {
+//     alert("Всі поля повинні бути заповнені!");
+//     return;
+//   }
+
+//   const formData = {
+//     email: emailInput.value,
+//     password: passwordInput.value,
+//   };
+
+//   console.log(formData);
+
+//   loginForm.reset();
+// });
+
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorSpan = document.querySelector(".change-color");
+
+changeColorSpan.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
